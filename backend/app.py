b9a23cb8d19e12274267ce46f1ce5a8a0e962af2
ProgrammerPouya <45574370+ProgrammerPouya@users.noreply.github.com>
2025-04-11 20:50:41 -0700

    chore: Back to debugging!

--- backend/app.py
+++ backend/app.py
@@ -329,4 +329,4 @@ def health():
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=5001, debug=True, use_reloader=False)
-    if "-v" in sys.argv: DEBUG = True
+    DEBUG = True #if "-v" in sys.argv: DEBUG = True
